c2f8d94cef3a28957b4a6f90aaad232d0d215797
sami.genius <sami.moinul@gmail.com>
2019-02-27 04:44:52 -0800

    Initial commit

---
+++ node-js_todo/public/assets/todo-list.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+  $('form').on('submit', function(){
+
+      var item = $('form input');
+      var todo = {item: item.val()};
+
+      $.ajax({
+        type: 'POST',
+        url: '/todo',
+        data: todo,
+        success: function(data){
+          //do something with the data via front-end framework
+          location.reload();
+        }
+      });
+
+      return false;
+
+  });
+
+  $('li').on('click', function(){
+      var item = $(this).text().replace(/ /g, "-");
+      $.ajax({
+        type: 'DELETE',
+        url: '/todo/' + item,
+        success: function(data){
+          //do something with the data via front-end framework
+          location.reload();
+        }
+      });
+  });
+
+});
+
+
+
+$(document).ready(function(){
+
+    var $checkboxes = $('#devel-generate-content-form td input[type="checkbox"]');
+
+    $checkboxes.change(function(){
+        var countCheckedCheckboxes = $checkboxes.filter(':checked').length;
+        $('#count-checked-checkboxes').text(countCheckedCheckboxes);
+    });
+
+});
